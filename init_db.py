0da97b156f20fb18156728000533d14c7db7f19a
Chandralim128 <chandralim128@gmail.com>
2023-09-29 14:01:38 +0700

    Update init_db.py
    final deploy

--- init_db.py
+++ init_db.py
@@ -1,7 +1,8 @@
 import sqlite3
 from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, ForeignKey, Float, Date
 
-DATABASE_URL = 'sqlite:///Stupla.db'
+# DATABASE_URL = 'sqlite:///Stupla.db'
+DATABASE_URL = 'mysql+mysqlconnector://root:ifapi3HmRFiGKYnwtYe0@containers-us-west-157.railway.app:5474/railway'
 engine = create_engine(DATABASE_URL, echo=True)
 metadata = MetaData()
 
